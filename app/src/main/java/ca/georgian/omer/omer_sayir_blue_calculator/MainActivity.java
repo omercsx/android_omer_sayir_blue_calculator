b7d9d96304c1e2fbc5d3a5e552c9e2261cb00140
omercsx <omercagris@gmail.com>
2024-09-13 11:43:22 -0400

    button clicked for numbers

--- app/src/main/java/ca/georgian/omer/omer_sayir_blue_calculator/MainActivity.java
+++ app/src/main/java/ca/georgian/omer/omer_sayir_blue_calculator/MainActivity.java
@@ -1,15 +1,42 @@
 package ca.georgian.omer.omer_sayir_blue_calculator;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    boolean acceptingDigits = true;
+    boolean startNewNumber = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void buttonClicked(android.view.View view) {
+
+        // Which button is pressed
+        Button button = (Button) view;
+        String buttonText = button.getText().toString();
+
+        // change display text
+        String displayText = (String) ((TextView) findViewById(R.id.numberDisplay)).getText();
+        TextView numberDisplay = (TextView) findViewById(R.id.numberDisplay);
+
+        if (buttonText.equals("C")) {
+            numberDisplay.setText("0");
+            return;
+        } else if (Character.isDigit(buttonText.charAt(0))) {
+            if (displayText.equals("0")) {
+                numberDisplay.setText(buttonText);
+            } else {
+                numberDisplay.setText(displayText + buttonText);
+            }
+        }
     }
 }
